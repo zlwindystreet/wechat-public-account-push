6dcf965ebe33053f2cb701ca1ae54c74a50e566c
zlwindystreet <116582857+zlwindystreet@users.noreply.github.com>
2022-10-26 19:47:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,60 +8,60 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx58d0a0e01484c9b2',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '0c32650272446885e76928deb45922b4',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '北京',
+  CITY: '北京',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小文裕',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      // 使用其他通道时，请严格按照各个通道的教程进行填写
+      id: 'oVEqa5mXsU-iUzSNjZZJJoxJPtxM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
+      useTemplateId: 'rsj_8lnAoMRL6_RlOjoHmhpoaIU7jbMHvRiu0Lo5vY4',
+      // 所在省份或城市，也可以不填
+      province: '北京',
+      // 所在城市或县区
+      city: '朝阳区',
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '08-01',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: 'https://astro.sina.com.cn/fate_day_leo/',
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '小文裕', year: '1998', date: '08-01',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '生日',name: '小文裕', year: '1998', date: '08-01',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
+  
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+      useTemplateId: '1toO_Tjryn4Lz12_yfrw1Bs0NefA9Ie8Sb7bu_mbVdY',
   CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oVEqa5mXsU-iUzSNjZZJJoxJPtxM',
     }
   ],
 
 }
 
 module.exports = USER_CONFIG
-
