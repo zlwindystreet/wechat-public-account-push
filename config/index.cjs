17a3129e55f10d0755d3af860a13d9adae18962a
zlwindystreet <116582857+zlwindystreet@users.noreply.github.com>
2022-10-26 20:34:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       id: 'oVEqa5mXsU-iUzSNjZZJJoxJPtxM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: '3lGXL4mlNFdQebRGZTgO9sL0XOYA_v-G6JcPJuKlAOk',
+      useTemplateId: 'U21uJQdL6PthuhXktQKiPt1zfX8Yc7Hpz262ZeprQ9M',
       // 所在省份或城市，也可以不填
       province: '北京',
       // 所在城市或县区
