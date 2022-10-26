d143e59ab6b4494d96ac38e02a13548ec80a828d
zlwindystreet <116582857+zlwindystreet@users.noreply.github.com>
2022-10-26 21:50:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       name: '小文裕',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      id: 'SCT178815T8Csyp2fYkyG7pcmM5qTABTrv',
+      id: 'SCT178815TwZ2YW4rcskKlNAvLQF1i5qlb',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: '0001',
