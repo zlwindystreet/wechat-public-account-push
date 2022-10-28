43435d35daffc73e749aff80f97fe8967d610c7d
zlwindystreet <116582857+zlwindystreet@users.noreply.github.com>
2022-10-29 01:08:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,6 +13,10 @@ const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
+  
+  province: '北京',
+      // 所在城市或县区
+      city: '北京朝阳区',
 
   
 
@@ -27,9 +31,7 @@ const USER_CONFIG = {
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: '0001',
       // 所在省份或城市，也可以不填
-      province: '北京',
-      // 所在城市或县区
-      city: '北京朝阳区',
+    
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '08-01',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
